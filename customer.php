f83aa47df5a85b40ee9386b6c0b8989febb23028
shahab gheysari <shahab.gheysari@rdsysco.com>
2018-07-22 14:02:18 +0430

    extract amountFor method

--- customer.php
+++ customer.php
@@ -25,7 +25,27 @@ class Customer{
         $result = "Rental Record for ".$this->getName()."\n";
         foreach($rentals as $each){
             $thisAmount = 0;
+            $thisAmount = $this->amountFor($each);            
+            // add frequent renter points
+            $frequentRenterPoints++;
+            // add bonus for a two day new release rental
+            if(($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE) && $each->getDaysRented() > 1)
+                $frequentRenterPoints++;
+            
+            //show figures for this rental
+            $result .= "\t".$each->getMovie()->getTitle()."\t".$thisAmount."\n";
+            $totalAmount += $thisAmount;
+        }
 
+        //add footer lines
+        $result .= "Amount owed is ".$totalAmount."\n";
+        $result .= "You earned ".$frequentRenterPoints." frequent renter point";
+        return $result;
+    }
+
+    private function amountFor($each){
+        $thisAmount = 0;
+        
             // determine amounts for each line
             /**
              * @var Rental $each
@@ -45,21 +65,6 @@ class Customer{
                         $thisAmount += ($each->getDaysRented() - 3) * 1.5;
                     break;
             }
-
-            // add frequent renter points
-            $frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if(($each->getMovie()->getPriceCode() == Movie::NEW_RELEASE) && $each->getDaysRented() > 1)
-                $frequentRenterPoints++;
-            
-            //show figures for this rental
-            $result .= "\t".$each->getMovie()->getTitle()."\t".$thisAmount."\n";
-            $totalAmount += $thisAmount;
-        }
-
-        //add footer lines
-        $result .= "Amount owed is ".$totalAmount."\n";
-        $result .= "You earned ".$frequentRenterPoints." frequent renter point";
-        return $result;
+        return $thisAmount;
     }
 }
