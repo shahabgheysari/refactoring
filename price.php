9e5ee73c2817fda7d43970e1c632bcf7b54a4049
shahab gheysari <shahab.gheysari@rdsysco.com>
2018-07-22 16:05:59 +0430

    moving getCharge method

--- price.php
+++ price.php
@@ -1,7 +1,29 @@
 <?php
 
 abstract class Price{
+    
     abstract function getPriceCode();
+
+    public function getCharge($daysRented){
+        $result = 0;
+        
+        switch($this->getPriceCode()){
+            case Movie::REGULAR:
+                $result += 2;
+                if($daysRented > 2)
+                    $result += ($daysRented - 2) * 1.5 ;
+                break;
+            case Movie::NEW_RELEASE:
+                $result += $daysRented * 3;
+                break;
+            case Movie::CHILDRENS:
+                $result += 1.5;
+                if($daysRented > 3)
+                    $result += ($daysRented - 3) * 1.5;
+                break;
+        }
+        return $result;
+    }
 }
 
 class ChildrensPrice extends Price {
