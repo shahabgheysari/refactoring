f347bc45ab5ae5374168ab10efa352e7587b92d5
shahab gheysari <shahab.gheysari@rdsysco.com>
2018-07-22 15:29:16 +0430

    moving getCharge to Movie

--- rental.php
+++ rental.php
@@ -18,33 +18,15 @@ class Rental{
         return $this->_movie;
     }
 
-    public function getCharge(){
-        $result = 0;
-        
-        switch($this->getMovie()->getPriceCode()){
-            case Movie::REGULAR:
-                $result += 2;
-                if($this->getDaysRented() > 2)
-                    $result += ($this->getDaysRented() - 2) * 1.5 ;
-                break;
-            case Movie::NEW_RELEASE:
-                $result += $this->getDaysRented() * 3;
-                break;
-            case Movie::CHILDRENS:
-                $result += 1.5;
-                if($this->getDaysRented() > 3)
-                    $result += ($this->getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return $result;
-
-    }
-
     public function getFrequentRenterPoints(){
         if(($this->getMovie()->getPriceCode() == Movie::NEW_RELEASE) && $this->getDaysRented() > 1)
             return 2;
         else 
             return 1;
+    }
+
+    public function getCharge(){
+        return $this->_movie->getCharge($this->_daysRented);
     }
 
 }
