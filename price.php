227361ba11f724c3f7a54b25be536ded6ebf9fcb
shahab gheysari <shahab.gheysari@rdsysco.com>
2018-07-22 16:22:02 +0430

    moving getFrequentRenterPoints, overriding getFrequentRenterPoints, change getCharge to abstract, implementing getChargee in subclasses

--- price.php
+++ price.php
@@ -1,28 +1,11 @@
 <?php
 
 abstract class Price{
-    
     abstract function getPriceCode();
+    abstract function getCharge($daysRented);
 
-    public function getCharge($daysRented){
-        $result = 0;
-        
-        switch($this->getPriceCode()){
-            case Movie::REGULAR:
-                $result += 2;
-                if($daysRented > 2)
-                    $result += ($daysRented - 2) * 1.5 ;
-                break;
-            case Movie::NEW_RELEASE:
-                $result += $daysRented * 3;
-                break;
-            case Movie::CHILDRENS:
-                $result += 1.5;
-                if($daysRented > 3)
-                    $result += ($daysRented - 3) * 1.5;
-                break;
-        }
-        return $result;
+    public function getFrequentRenterPoints($daysRented){
+        return 1;
     }
 }
 
@@ -30,16 +13,39 @@ class ChildrensPrice extends Price {
     function getPriceCode() {
         return Movie::CHILDRENS;
     }
+
+    public function getCharge($daysRented){
+        $result = 1.5;
+        if ($daysRented > 3)
+            $result += ($daysRented - 3) * 1.5;
+        return $result;
+    }
+
 }
 
 class NewReleasePrice extends Price {
     function getPriceCode() {
         return Movie::NEW_RELEASE;
     }
+
+    public function getCharge($daysRented){
+        return $daysRented * 3;
+    }
+
+    public function getFrequentRenterPoints($daysRented){
+        return ($daysRented > 1) ? 2 : 1;
+    }
 }
 
 class RegularPrice extends Price {
     function getPriceCode() {
         return Movie::REGULAR;
+    }
+
+    public function getCharge($daysRented){
+        $result = 2;
+        if($daysRented > 2)
+            $result += ($daysRented - 2) * 1.5 ;
+        return $result;
     }
 }
